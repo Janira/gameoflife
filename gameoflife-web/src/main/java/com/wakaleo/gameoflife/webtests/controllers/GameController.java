588f35fcbb099fa9e048ed0bd53c0fb2c3315e3d
Juan Fernández-Corugedo Igual <juan.fernandez-corugedo.contractor@bbva.com>
2014-05-14 09:51:47 +0200

    Fix major violations

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -56,7 +56,7 @@ public class GameController {
         try {
             Thread.currentThread().sleep(thinkingTime);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            /*DO NOTHING*/
         }
     }
 
